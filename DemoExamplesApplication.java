b4842c67e9689f6008a320ea13a2ea0b97960f59
TL <TL@gmail.com>
2024-08-15 20:52:10 +0530

    modified in devloper22

--- DemoExamplesApplication.java
+++ DemoExamplesApplication.java
@@ -9,6 +9,7 @@ public class DemoExamplesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoExamplesApplication.class, args);
 		System.out.println("BasePrograms in ");
+		int w=200;
 	}
 
 }
